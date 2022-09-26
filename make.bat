1fcc04e35b9220fd43cdc71d1ed2afb42a6bc033
moth1995 <marcos-flores@live.com.ar>
2022-09-26 03:20:43 -0300

    adding icon to the .exe

--- make.bat
+++ make.bat
@@ -6,7 +6,7 @@ set FILE_VERSION=file_version_info.txt
 set EXTRA_ARG=--add-data=resources/*;resources --additional-hooks-dir=. 
 set ICO_DIR=resources/pes_indie.ico
 
-pyinstaller --onefile --window "%PY_FILE%" --name "%PROJECT_NAME%_%VERSION%"  %EXTRA_ARG% --version-file "%FILE_VERSION%"
+pyinstaller --onefile --icon="%ICO_DIR%" --window "%PY_FILE%" --name "%PROJECT_NAME%_%VERSION%"  %EXTRA_ARG% --version-file "%FILE_VERSION%"
 
 cd dist
 tar -acvf "%PROJECT_NAME%_%VERSION%.zip" *
